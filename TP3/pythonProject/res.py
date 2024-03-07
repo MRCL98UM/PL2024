41788b7fb368416c304b978aba19761f5ff6dbbc
MRCL98UM <80057819+MRCL98UM@users.noreply.github.com>
2024-03-07 19:12:28 +0000

    Update res.py

--- TP3/pythonProject/res.py
+++ TP3/pythonProject/res.py
@@ -3,3 +3,40 @@
 #2Sempre que encontrar a string “Off” em qualquer combinação de maiúsculas e minúsculas, esse comportamento é desligado;
 #3Sempre que encontrar a string “On” em qualquer combinação de maiúsculas e minúsculas, esse comportamento é novamente ligado;
 #4Sempre que encontrar o caráter “=”, o resultado da soma é colocado na saída.
+import re
+
+def somador(texto):
+    soma =0
+    on = True
+    lista_numeros=[]
+
+    for line in texto:
+        tokens = re.finditer(r'OFF|ON|=|[+\-]?\d+', line,re.IGNORECASE)
+        for token in tokens:
+            if on == True:
+                if token.group().upper() == 'ON':
+                    on = True
+                elif token.group().upper() == 'OFF':
+                    on = False
+                elif token.group().upper() == '=':
+                    print(soma)
+                else:
+                    soma = soma + int(token.group())
+            elif on == False:
+                if token.group().upper() == 'ON':
+                    on = True
+                elif token.group().upper() == 'OFF':
+                    on = False
+                elif token.group().upper() == '=':
+                    print(soma)
+                else:
+                    pass
+
+
+def main():
+    texto = ["ON 123 456","OFF 123 456","ON 123 456","= 123 456"]
+    print(texto)
+    somador(texto)
+
+if __name__ == "__main__":
+    main()
